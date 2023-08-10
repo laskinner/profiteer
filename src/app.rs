33e30b390f772701d8d4ef244f8475c10703fb53
Luke Skinner <laskinner@gmail.com>
2023-08-11 01:16:09 +0200

    small refactoring

--- src/app.rs
+++ src/app.rs
@@ -39,53 +39,32 @@ fn HomePage(cx: Scope) -> impl IntoView {
     
     view! { cx,
         <h1>"Welcome to Profiteer"</h1>
-         <form><label>"Enter the number of customers at the start of the period "
-            <input type="text"
-        on:input=move |ev| {
-            // event_target_value is a Leptos helper function
-            // it functions the same way as event.target.value
-            // in JavaScript, but smooths out some of the typecasting
-            // necessary to make this work in Rust
+        
+        <form><label>"Enter the number of customers at the start of the period "
+            <input type="text" on:input=move |ev| {
             set_customers_start(event_target_value(&ev));
-        }
-
-        // the `prop:` syntax lets you update a DOM property,
-        // rather than an attribute.
-        prop:value=customers_start
-    />
-            </label></form>
+        } prop:value=customers_start
+        />
+        </label></form>
+        
         <form><label>"Enter the number of customers at the en of the period "
-            <input type="text"
-        on:input=move |ev| {
-            // event_target_value is a Leptos helper function
-            // it functions the same way as event.target.value
-            // in JavaScript, but smooths out some of the typecasting
-            // necessary to make this work in Rust
+            <input type="text" on:input=move |ev| {
             set_customers_end(event_target_value(&ev));
-        }
-
-        // the `prop:` syntax lets you update a DOM property,
-        // trather than an attribute.
-        prop:value=customers_end
-    /></label></form>
-<form><label>"Enter the number of customers added during the period "
-            <input type="text"
-        on:input=move |ev| {
-            // event_target_value is a Leptos helper function
-            // it functions the same way as event.target.value
-            // in JavaScript, but smooths out some of the typecasting
-            // necessary to make this work in Rust
+        } prop:value=customers_end
+        />
+        </label></form>
+    
+        <form><label>"Enter the number of customers added during the period "
+            <input type="text" on:input=move |ev| {
             set_customers_added(event_target_value(&ev));
-        }
-
-        // the `prop:` syntax lets you update a DOM property,
-        // rather than an attribute.
-        prop:value=customers_added
-    /></label></form>
- <p>"Number of Customers at start of period: " {customers_start}</p>
- <p>"Number of Customers at end of period: " {customers_end}</p>
- <p>"Number of Customers added during period: " {customers_added}</p>
-}   
+        } prop:value=customers_added
+        />
+        </label></form>
+ 
+        <p>"Number of Customers at start of period: " {customers_start}</p>
+        <p>"Number of Customers at end of period: " {customers_end}</p>
+        <p>"Number of Customers added during period: " {customers_added}</p>
+    }   
 }
 
 
