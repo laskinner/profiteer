c400139928416cfc770ac636d73dd7901e554ad1
laskinner <laskinner@gmail.com>
2023-08-10 02:46:58 +0200

    combined the new profiteer with old profiteer

--- src/app.rs
+++ src/app.rs
@@ -32,18 +32,63 @@ pub fn App(cx: Scope) -> impl IntoView {
 #[component]
 fn HomePage(cx: Scope) -> impl IntoView {
     // Creates a reactive value to update the button
-    let (count, set_count) = create_signal(cx, 0);
-    let on_click = move |_| set_count.update(|count| *count += 1);
-
+    
+    let (customers_start, set_customers_start) = create_signal(cx, "Enter number".to_string());
+    let (customers_end, set_customers_end) = create_signal(cx, "Enter number".to_string());
+    let (customers_added, set_customers_added) = create_signal(cx, "Enter number".to_string());
+    
     view! { cx,
-        <h1>"Welcome to Profiteer and fuck you cockboy"</h1>
-        <h2>"Enter your information below to log in."</h2>
-        <button on:click=on_click>"Click Me: " {count}</button>
-        <h3>"First Name"</h3>
-        <input type="text" fname="fname"></input>
-        <h3>"Last Name"</h3>
-        <input type="text" lname="lname"></input>
-        <h3>"Email"</h3>
-        <input type="email" email="email"></input>
-    }
+        <h1>"Welcome to Profiteer"</h1>
+         <form><label>"Enter the number of customers at the start of the period "
+            <input type="text"
+        on:input=move |ev| {
+            // event_target_value is a Leptos helper function
+            // it functions the same way as event.target.value
+            // in JavaScript, but smooths out some of the typecasting
+            // necessary to make this work in Rust
+            set_customers_start(event_target_value(&ev));
+        }
+
+        // the `prop:` syntax lets you update a DOM property,
+        // rather than an attribute.
+        prop:value=customers_start
+    />
+            </label></form>
+        <form><label>"Enter the number of customers at the en of the period "
+            <input type="text"
+        on:input=move |ev| {
+            // event_target_value is a Leptos helper function
+            // it functions the same way as event.target.value
+            // in JavaScript, but smooths out some of the typecasting
+            // necessary to make this work in Rust
+            set_customers_end(event_target_value(&ev));
+        }
+
+        // the `prop:` syntax lets you update a DOM property,
+        // trather than an attribute.
+        prop:value=customers_end
+    /></label></form>
+<form><label>"Enter the number of customers added during the period "
+            <input type="text"
+        on:input=move |ev| {
+            // event_target_value is a Leptos helper function
+            // it functions the same way as event.target.value
+            // in JavaScript, but smooths out some of the typecasting
+            // necessary to make this work in Rust
+            set_customers_added(event_target_value(&ev));
+        }
+
+        // the `prop:` syntax lets you update a DOM property,
+        // rather than an attribute.
+        prop:value=customers_added
+    /></label></form>
+ <p>"Number of Customers at start of period: " {customers_start}</p>
+ <p>"Number of Customers at end of period: " {customers_end}</p>
+ <p>"Number of Customers added during period: " {customers_added}</p>
+}   
 }
+
+
+
+
+
