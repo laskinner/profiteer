ccdec95084401efebe5957c8591b8218ecd62d50
Luke Skinner <laskinner@gmail.com>
2023-08-14 01:38:15 +0200

    churn-rate-built

--- src/app.rs
+++ src/app.rs
@@ -31,32 +31,35 @@ pub fn App(cx: Scope) -> impl IntoView {
 /// Renders the home page of your application.
 #[component]
 fn HomePage(cx: Scope) -> impl IntoView {
-    // Creates a reactive value to update the button
+    // Churn Rate 
     
-    let (customers_start, set_customers_start) = create_signal(cx, "Enter value".to_string());
-    let (customers_end, set_customers_end) = create_signal(cx, "Enter value".to_string());
-    let (customers_added, set_customers_added) = create_signal(cx, "Enter value".to_string());
-   
+    let (customers_start, set_customers_start) = create_signal(cx, 1);
+    let (customers_end, set_customers_end) = create_signal(cx, 0);
+    let (customers_added, set_customers_added) = create_signal(cx, 0);
+    let churn = move || (customers_start() - customers_end()) / customers_start() * 100;
+    
+    // Revenue Churn
+    
     view! { cx,
         <h1>"Welcome to Profiteer"</h1>
         
         <form><label>"Enter the number of customers at the start of the period "
             <input type="text" on:input=move |ev| {
-            set_customers_start(event_target_value(&ev));
+            set_customers_start(event_target_value(&ev).parse::<i32>().unwrap());
         } prop:value=customers_start
         />
         </label></form>
         
-        <form><label>"Enter the number of customers at the en of the period "
+        <form><label>"Enter the number of customers at the end of the period "
             <input type="text" on:input=move |ev| {
-            set_customers_end(event_target_value(&ev));
+            set_customers_end(event_target_value(&ev).parse::<i32>().unwrap());
         } prop:value=customers_end
         />
         </label></form>
     
         <form><label>"Enter the number of customers added during the period "
             <input type="text" on:input=move |ev| {
-            set_customers_added(event_target_value(&ev));
+            set_customers_added(event_target_value(&ev).parse::<i32>().unwrap());
         } prop:value=customers_added
         />
         </label></form>
@@ -64,10 +67,8 @@ fn HomePage(cx: Scope) -> impl IntoView {
         <p>"Number of Customers at start of period: " {customers_start}</p>
         <p>"Number of Customers at end of period: " {customers_end}</p>
         <p>"Number of Customers added during period: " {customers_added}</p>
+
+
+        <p>"Churn rate: " {churn}"%"</p>
     }   
 }
-
-
-
-
-
