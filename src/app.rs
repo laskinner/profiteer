591e96d6392ade5e45c6223f4155f1d9c7bc1157
Luke Skinner <laskinner@gmail.com>
2023-08-23 01:57:30 +0200

    Styling

--- src/app.rs
+++ src/app.rs
@@ -12,14 +12,17 @@ pub fn App(cx: Scope) -> impl IntoView {
         // injects a stylesheet into the document <head>
         // id=leptos means cargo-leptos will hot-reload this stylesheet
         <Stylesheet id="leptos" href="/pkg/leptos_start.css"/>
-
+        <nav class="bg-slate-800">
+          <a href="www.google.com" class="text-slate-300 px-4 py-2">"Home"</a>
+//          <a href="www.google.com" class="text-gray-700 px-4 py-2">Contact</a>
+        </nav>
         // sets the document title
         <Title text="Profiteer"/>
 
-        <body class="bg-gray-900/90">
+        <body class="bg-slate-950">
             // content for this welcome page
-            <h1 class="text-center text-xl text-white">"Welcome to Profiteer"</h1>
-            <h2 class="text-center text-xl text-white">"Instructions: Provide values for the business period."</h2>
+            <h1 class="text-center text-4xl text-indigo-300 pt-4">"Welcome to Profiteer"</h1>
+            <h2 class="text-center text-xl text-indigo-300 pb-10">"Instructions: Provide values for the business period."</h2>
             <Router>
                 <main>
                     <Routes>
@@ -48,32 +51,36 @@ fn HomePage(cx: Scope) -> impl IntoView {
     view! { cx,
         <main> 
             <div class="grid grid-cols-2 gap-4">
-                <div class="mp-6 p-6 mx-auto rounded-xl items-center space-x-4 text-lime-400 bg-violet-600/50">
+                <div class="mp-6 p-6 mx-auto rounded-xl items-center space-x-4 text-slate-900 bg-indigo-600">
+
                     <p class="text-2xl text-center">"Beginning of period"</p>
 
-                        <label>"Number of customers "
-                            <input type="text" class="float-right" on:input=move |ev| {
+                    <div class="flex float-right">
+                        <label for="input" class="mr-2">"Number of customers:"</label>
+                            <input type="text" class="flex-shrink rounded border text-slate-800 bg-indigo-300" on:input=move |ev| {
                             set_customers_start(event_target_value(&ev).parse::<i32>().unwrap());
                             } prop:value=customers_start/>
-                        </label>
+                    </div>
 
                     <p class="text-2xl text-center">"End of period"</p>
                     
-                        <label>"Number of Customers "
-                            <input type="text" class="float-right" on:input=move |ev| {
+                    <div class="flex float-right">
+                        <label for="input" class="mr-2">"Number of Customers:"</label>
+                            <input type="text" class="flex-shrink rounded border text-slate-800 bg-indigo-300" on:input=move |ev| {
                             set_customers_end(event_target_value(&ev).parse::<i32>().unwrap());
                             } prop:value=customers_end/>
-                        </label>
+                    </div>
                     
                     <p class="text-2xl text-center">"During the period"</p>
 
-                        <label>"Customers added "
-                            <input type="text" class="float-right" on:input=move |ev| {
+                    <div class="flex float-right">
+                        <label for="input" class="mr-2">"Customers added:"</label>
+                            <input type="text" class="flex-shrink rounded border text-slate-800 bg-indigo-300" on:input=move |ev| {
                             set_customers_added(event_target_value(&ev).parse::<i32>().unwrap());
                             } prop:value=customers_added/>
-                        </label>
-                </div>
-            <div class="mp-6 mx-auto rounded-xl items-center space-x-4 text-lime-400 bg-violet-600/50">
+                    </div>
+            </div>
+            <div class="mp-6 mx-auto rounded-xl items-center space-x-4 text-slate-900 bg-indigo-600">
                 <ul class="p-6 divide-y divide-slate-200 text-right"> 
                     <p>"Current Customers: " {customers_end}</p>
                     <p>"New Customers: " {customers_added}</p>
