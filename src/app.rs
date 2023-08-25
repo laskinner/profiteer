920295ed88f2dc3b0db3a572b53aa1ca6695ef53
Luke Skinner <laskinner@gmail.com>
2023-08-25 14:34:37 +0200

    more styling

--- src/app.rs
+++ src/app.rs
@@ -12,17 +12,28 @@ pub fn App(cx: Scope) -> impl IntoView {
         // injects a stylesheet into the document <head>
         // id=leptos means cargo-leptos will hot-reload this stylesheet
         <Stylesheet id="leptos" href="/pkg/leptos_start.css"/>
-        <nav class="bg-slate-800">
-          <a href="www.google.com" class="text-slate-300 px-4 py-2">"Home"</a>
-//          <a href="www.google.com" class="text-gray-700 px-4 py-2">Contact</a>
-        </nav>
+            <nav class="bg-gradient-to-r from-cyan-300 to-blue-600">
+              <div class="container mx-auto px-4">
+                <div class="flex items-center justify-between h-16">
+                  <div class="flex items-center">
+                    <a href="images.com" class="text-white font-bold text-lg">"Profiteer"</a>
+                  </div>
+                  <div class="flex items-center">
+                    <a href="www.google.com" class="text-white hover:text-gray-300 px-3 py-2">"Home"</a>
+                    <a href="www.facebook.com" class="text-white hover:text-gray-300 px-3 py-2">"About"</a>
+                    <a href="www.openai.com" class="text-white hover:text-gray-300 px-3 py-2">"Services"</a>
+                    <a href="www.bing.com" class="text-white hover:text-gray-300 px-3 py-2">"Contact"</a>
+                  </div>
+                </div>
+              </div>
+            </nav>
         // sets the document title
         <Title text="Profiteer"/>
 
         <body class="bg-slate-950">
             // content for this welcome page
-            <h1 class="text-center text-4xl text-indigo-300 pt-4">"Welcome to Profiteer"</h1>
-            <h2 class="text-center text-xl text-indigo-300 pb-10">"Instructions: Provide values for the business period."</h2>
+            <h1 class="text-center text-4xl text-white pt-4">"Welcome to Profiteer"</h1>
+            <h2 class="text-center text-xl text-white pb-10">"Instructions: Provide values for the business period."</h2>
             <Router>
                 <main>
                     <Routes>
@@ -49,15 +60,15 @@ fn HomePage(cx: Scope) -> impl IntoView {
     // Revenue Churn
     
     view! { cx,
-        <main> 
+        <main>
             <div class="grid grid-cols-2 gap-4">
-                <div class="mp-6 p-6 mx-auto rounded-xl items-center space-x-4 text-slate-900 bg-indigo-600">
+                <div class="mp-6 p-6 mx-auto rounded-xl items-center space-x-4 text-white bg-indigo-600">
 
                     <p class="text-2xl text-center">"Beginning of period"</p>
 
                     <div class="flex float-right">
                         <label for="input" class="mr-2">"Number of customers:"</label>
-                            <input type="text" class="flex-shrink rounded border text-slate-800 bg-indigo-300" on:input=move |ev| {
+                            <input type="text" class="flex-shrink rounded border text-white bg-indigo-300" on:input=move |ev| {
                             set_customers_start(event_target_value(&ev).parse::<i32>().unwrap());
                             } prop:value=customers_start/>
                     </div>
@@ -66,7 +77,7 @@ fn HomePage(cx: Scope) -> impl IntoView {
                     
                     <div class="flex float-right">
                         <label for="input" class="mr-2">"Number of Customers:"</label>
-                            <input type="text" class="flex-shrink rounded border text-slate-800 bg-indigo-300" on:input=move |ev| {
+                            <input type="text" class="flex-shrink rounded border text-white bg-indigo-300" on:input=move |ev| {
                             set_customers_end(event_target_value(&ev).parse::<i32>().unwrap());
                             } prop:value=customers_end/>
                     </div>
@@ -75,12 +86,12 @@ fn HomePage(cx: Scope) -> impl IntoView {
 
                     <div class="flex float-right">
                         <label for="input" class="mr-2">"Customers added:"</label>
-                            <input type="text" class="flex-shrink rounded border text-slate-800 bg-indigo-300" on:input=move |ev| {
+                            <input type="text" class="flex-shrink rounded border text-white bg-indigo-300" on:input=move |ev| {
                             set_customers_added(event_target_value(&ev).parse::<i32>().unwrap());
                             } prop:value=customers_added/>
                     </div>
             </div>
-            <div class="mp-6 mx-auto rounded-xl items-center space-x-4 text-slate-900 bg-indigo-600">
+            <div class="mp-6 mx-auto rounded-xl items-center space-x-4 text-white bg-indigo-600">
                 <ul class="p-6 divide-y divide-slate-200 text-right"> 
                     <p>"Current Customers: " {customers_end}</p>
                     <p>"New Customers: " {customers_added}</p>
